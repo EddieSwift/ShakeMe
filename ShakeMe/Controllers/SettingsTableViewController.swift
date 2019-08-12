a3307042ddc64ac755b796a2bf8fff60514c893a
Eduard Galchenko <eddiegalchenko@gmail.com>
2019-08-12 23:46:29 +0300

    Added the ability to delete custom answers
    - Delete rows with fade animation in delete editingStyle
    - Core Data funcs optimization

--- ShakeMe/Controllers/SettingsTableViewController.swift
+++ ShakeMe/Controllers/SettingsTableViewController.swift
@@ -22,51 +22,39 @@ class SettingsTableViewController: UITableViewController {
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
-
-        guard let appDelegate =
-            UIApplication.shared.delegate as? AppDelegate else {
-                return
-        }
-        
-        let managedContext =
-            appDelegate.persistentContainer.viewContext
-
-        let fetchRequest =
-            NSFetchRequest<NSManagedObject>(entityName: "CustomAnswer")
-
-        do {
-            answers = try managedContext.fetch(fetchRequest)
-        } catch let error as NSError {
-            print("Could not fetch. \(error), \(error.userInfo)")
-        }
+        fetchAllAnswers()
     }
     
     // MARK: - Actions
     
     @IBAction func addCustomAnswer(_ sender: UIBarButtonItem) {
         
-        let alert = UIAlertController(title: "New answer", message: "Add a new custom answer", preferredStyle: .alert)
-
+        let alert = UIAlertController(title: "New answer", message: "Add a new custom answer.", preferredStyle: .alert)
+        
         let saveAction = UIAlertAction(title: "Save", style: .default) { (UIAlertAction) in
-
+            
             guard let textField = alert.textFields?.first,
                 let answerToSave = textField.text else {
                     return
             }
             
-            if answerToSave.count < 1 { self.guardAlert() }
+            // Show alert if try save empty string answer
+            if answerToSave.count < 1 {
+                self.emptyStringAlert()
+                return
+            }
             
             self.save(answer: answerToSave)
             self.tableView.reloadData()
         }
-
+        
         let cancleAction = UIAlertAction(title: "Cancel", style: .cancel)
-
+        
         alert.addTextField()
-
+        
         alert.addAction(saveAction)
         alert.addAction(cancleAction)
-
+        
         present(alert, animated: true)
     }
     
@@ -94,81 +82,85 @@ class SettingsTableViewController: UITableViewController {
         }
     }
     
+    func deleteAnswer(answer: NSManagedObject) {
+        
+        guard let appDelegate =
+            UIApplication.shared.delegate as? AppDelegate else {
+                return
+        }
+        
+        let managedContext =
+            appDelegate.persistentContainer.viewContext
+        
+        managedContext.delete(answer)
+        
+        do {
+            try managedContext.save()
+        } catch let error as NSError {
+            print("Error While Deleting Note: \(error.userInfo)")
+        }
+    }
+    
+    func fetchAllAnswers() {
+        
+        guard let appDelegate =
+            UIApplication.shared.delegate as? AppDelegate else {
+                return
+        }
+        
+        let managedContext =
+            appDelegate.persistentContainer.viewContext
+        
+        let fetchRequest =
+            NSFetchRequest<NSManagedObject>(entityName: "CustomAnswer")
+        
+        do {
+            answers = try managedContext.fetch(fetchRequest)
+        } catch let error as NSError {
+            print("Could not fetch. \(error), \(error.userInfo)")
+        }
+    }
+    
     // MARK: - Help metods
     
-    func guardAlert() {
-        let alert = UIAlertController(title: "Error", message: "Answer should be at least one character or more. Try again, please.", preferredStyle: .alert)
-
+    func emptyStringAlert() {
+        let alert = UIAlertController(title: "Warning", message: "Answer should be at least one character or more. Try again, please.", preferredStyle: .alert)
+        
         let cancleAction = UIAlertAction(title: "Ok", style: .cancel)
         alert.addAction(cancleAction)
         
         present(alert, animated: true)
     }
-
+    
     // MARK: - UITableViewDataSource
-
+    
     override func numberOfSections(in tableView: UITableView) -> Int {
         return 1
     }
-
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return answers.count
     }
-
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
         let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
         let answer = answers[indexPath.row]
         
         cell.textLabel?.text = answer.value(forKey: "answer") as? String
-
+        
         return cell
     }
-
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
+    
     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        
         if editingStyle == .delete {
-            // Delete the row from the data source
+            
+            let answer = answers[indexPath.row]
+            deleteAnswer(answer: answer)
+            fetchAllAnswers()
+            
             tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
+        }
     }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
