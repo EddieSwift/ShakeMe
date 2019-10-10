7229a513b417fa8591a8134e49b7ee7da8ddc2e8
Eduard Galchenko <eddiegalchenko@gmail.com>
2019-10-10 17:24:07 +0300

    core data new attributes in entity

--- ShakeMe/Data/CustomAnswer+CoreDataClass.swift
+++ ShakeMe/Data/CustomAnswer+CoreDataClass.swift
@@ -2,13 +2,15 @@
 //  CustomAnswer+CoreDataClass.swift
 //  ShakeMe
 //
-//  Created by Eduard Galchenko on 8/13/19.
+//  Created by Eduard Galchenko on 10/10/19.
 //  Copyright © 2019 Eduard Galchenko. All rights reserved.
 //
 //
 
+import Foundation
 import CoreData
 
 @objc(CustomAnswer)
 public class CustomAnswer: NSManagedObject {
+
 }
--- ShakeMe/Data/CustomAnswer+CoreDataProperties.swift
+++ ShakeMe/Data/CustomAnswer+CoreDataProperties.swift
@@ -2,21 +2,28 @@
 //  CustomAnswer+CoreDataProperties.swift
 //  ShakeMe
 //
-//  Created by Eduard Galchenko on 8/13/19.
+//  Created by Eduard Galchenko on 10/10/19.
 //  Copyright © 2019 Eduard Galchenko. All rights reserved.
 //
 //
 
+import Foundation
 import CoreData
 
 extension CustomAnswer {
-    @NSManaged public var answerText: String?
 
     @nonobjc public class func fetchRequest() -> NSFetchRequest<CustomAnswer> {
         return NSFetchRequest<CustomAnswer>(entityName: "CustomAnswer")
     }
 
+    @NSManaged public var answerText: String?
+    @NSManaged public var answerDate: Date?
+    @NSManaged public var answerId: String?
+
     func toAnswer() -> Answer {
         return Answer(answerText: answerText ?? "")
+    }
+    func createAnswer() {
+        answerDate = Date()
     }
 }
