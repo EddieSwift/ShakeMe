b910ab2f677164259c87839bf20db1e3774594a7
Eduard Galchenko <eddiegalchenko@gmail.com>
2019-10-22 21:45:53 +0300

    RxSwift and RxCocoa imports in classes

--- ShakeMe/Presentation/Views/AnswerTableViewCell.swift
+++ ShakeMe/Presentation/Views/AnswerTableViewCell.swift
@@ -7,6 +7,8 @@
 //
 
 import UIKit
+import RxSwift
+import RxCocoa
 import SnapKit
 
 class AnswerTableViewCell: UITableViewCell {
--- ShakeMe/Presentation/Views/SettingsTableViewController.swift
+++ ShakeMe/Presentation/Views/SettingsTableViewController.swift
@@ -7,6 +7,8 @@
 //
 
 import UIKit
+import RxSwift
+import RxCocoa
 
 let rowHeight: CGFloat = 80.0
 
